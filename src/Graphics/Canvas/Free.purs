d0efc2e58552a0ed3bf7affeec42b237b12d27f2
Matt Kaemmerer <mattk@spiceworks.com>
2015-03-23 11:59:49 -0500

    createImageData

--- src/Graphics/Canvas/Free.purs
+++ src/Graphics/Canvas/Free.purs
@@ -42,6 +42,7 @@ module Graphics.Canvas.Free
   , getImageData
   , putImageData
   , putImageDataFull
+  , createImageData
 
   , runGraphics
   ) where
@@ -87,6 +88,7 @@ data GraphicsF more
   | GetImageData      Number      Number      Number    Number   (C.ImageData -> more)
   | PutImageData      C.ImageData Number      Number    more
   | PutImageDataFull  C.ImageData Number      Number    Number   Number  Number  Number  more
+  | CreateImageData   Number      Number      more
 
 type Graphics a = FreeC GraphicsF a
 
@@ -192,6 +194,9 @@ putImageData d x y = liftFC $ PutImageData d x y unit
 putImageDataFull :: C.ImageData -> Number -> Number -> Number -> Number -> Number -> Number -> Graphics Unit
 putImageDataFull d x y dx dy dw dh = liftFC $ PutImageDataFull d x y dx dy dw dh unit
 
+createImageData :: Number -> Number -> Graphics Unit
+createImageData w h = liftFC $ CreateImageData w h unit
+
 runGraphics :: forall a eff. C.Context2D -> Graphics a -> Eff (canvas :: C.Canvas | eff) a
 runGraphics ctx = runFreeCM interp
   where
@@ -230,4 +235,4 @@ runGraphics ctx = runFreeCM interp
   interp (GetImageData x y w h k)               = k <$> C.getImageData ctx x y w h
   interp (PutImageData d x y a)                 = const a <$> C.putImageData ctx d x y
   interp (PutImageDataFull d x y dx dy dw dh a) = const a <$> C.putImageDataFull ctx d x y dx dy dw dh
-
+  interp (CreateImageData w h a)                = const a <$> C.createImageData ctx w h
